15856d4eb74d499dcac6dba9f16c666469335acd
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-01-17 07:49:44 +0530

    CREATE: mobile nav open created

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const navLinks = document.querySelector(".nav-links");
+const menuOpen = document.querySelector(".open-menu");
+const menuClose = document.querySelector(".close-menu");
+const body = document.querySelector("body");
+
+menuOpen.addEventListener("click", () => {
+  navLinks.classList.add("nav-open");
+  menuOpen.style.display = "none";
+  menuClose.style.display = "block";
+});
+
+menuClose.addEventListener("click", () => {
+  navLinks.classList.remove("nav-open");
+  menuOpen.style.display = "block";
+  menuClose.style.display = "none";
+});
